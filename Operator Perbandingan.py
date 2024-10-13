ccbd2d52e15e198cea7c689fefa5576f34421b7c
Dwi Bakti N Dev / Roy <166002502+DwiDevelopes@users.noreply.github.com>
2024-10-13 14:16:44 +0700

    Add files via upload

---
+++ Operator Perbandingan.py
@@ -0,0 +1,17 @@
+# Ini Adalah Operator Perbandingan Compration
+print ("=====================================================================")
+masukandata1 = int(input("Masukan Angka Yang Ingin Anda Masukann : "))
+masukandata2 = int(input("Masukan Angka Yng Ingin Anda Masukan : "))
+print ("=====================================================================")
+masukandataif = masukandata1 > masukandata2
+masukandataif2 = masukandata1 < masukandata2
+masukandataif3 = masukandata1 == masukandata2
+masukandataif4 = masukandata1 != masukandata2
+
+print (masukandata1 , ">" , masukandata2 ,"= " , (masukandataif))
+print ("=====================================================================")
+print (masukandata1 , "<" , masukandata2 ,"=" , (masukandataif2))
+print ("=====================================================================")
+print (masukandata1 , "==" , masukandata2 ,"= " , (masukandataif3))
+print ("=====================================================================")
+print (masukandata1 , "!=" , masukandata2 ,"= " , (masukandataif4))
---
+++ perkalian.py
@@ -0,0 +1,24 @@
+print ("===================== pertambahan =============================")
+pertambahan = float(input("masukan angka pertambahan : "))
+pertambahan2 = float(input("masukan angka pertambahan : "))
+keluar = pertambahan + pertambahan2
+print (f"hasil nya dari pertambahan : {keluar}")
+print ("===============================================================")
+print ("===================== pengurangan =============================")
+pertambahan = float(input("masukan angka pengurangan : "))
+pertambahan2 = float(input("masukan angka pengurangan : "))
+keluar = pertambahan - pertambahan2
+print (f"hasil nya dari pengurangan : {keluar}")
+print ("===============================================================")
+print ("===================== perkalian ===============================")
+pertambahan = float(input("masukan angka perkalian : "))
+pertambahan2 = float(input("masukan angka perkalian : "))
+keluar = pertambahan * pertambahan2
+print (f"hasil nya dari perkalian : {keluar}")
+print ("===============================================================")
+print ("===================== pembagian ===============================")
+pertambahan = float(input("masukan angka pembagian : "))
+pertambahan2 = float(input("masukan angka pembagian : "))
+keluar = pertambahan / pertambahan2
+print (f"hasil nya dari pembagian : {keluar}")
+print ("===============================================================")
